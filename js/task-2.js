5846e517a64136b3d1929d531aa7b15233ef25e7
volodymyrbaboshyn21 <volodymyrbaboshyn@gmail.com>
2020-03-17 22:56:36 +0200

    js

--- js/task-2.js
+++ js/task-2.js
@@ -3,18 +3,18 @@ const inventory = {
     add(itemName) {
       console.log(`Adding ${itemName} to inventory`);
   
-      inventory.items.push(itemName);
+      this.items.push(itemName);
     },
     remove(itemName) {
       console.log(`Removing ${itemName} from inventory`);
   
-      inventory.items = inventory.items.filter(item => item !== itemName);
+      this.items = inventory.items.filter(item => item !== itemName);
     },
   };
   
   const invokeInventoryAction = function(itemName, action) {
     console.log(`Invoking action on ${itemName}`);
-    action(itemName);
+    action.call(inventory, itemName);
   };
   
   invokeInventoryAction('Medkit', inventory.add);
@@ -28,3 +28,30 @@ const inventory = {
   // Removing Gas mask from inventory
   
   console.log(inventory.items); // ['Knife', 'Medkit']
+
+
+
+  // const inventory = {
+  //   items: ['Монорельса', 'Фильтр'],
+  //   add(itemName) {
+  //     this.items.push(itemName);
+  //   },
+  //   remove(itemName) {
+  //     this.items = this.items.filter(item => item !== itemName);
+  //   },
+  // };
+  
+  // const invokeInventoryOperation = function(itemName, inventoryAction) {
+  //   console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  //   inventoryAction.call(inventory, itemName);
+  // };
+  
+  // invokeInventoryOperation('Аптечка', inventory.add);
+  // // Invoking add opeartion on Аптечка
+  
+  // console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+  
+  // invokeInventoryOperation('Фильтр', inventory.remove);
+  // // Invoking remove opeartion on Фильтр
+  
+  // console.log(inventory.items); // ['Монорельса', 'Аптечка']
